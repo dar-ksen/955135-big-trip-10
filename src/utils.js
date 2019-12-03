d4d123bfd1768b05688837cb2e2e6678426a0fcc
Dar <ksen.vl.set@gmail.com>
2019-12-03 22:46:39 +0300

    Rewrote the render function

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const getTwoDigitFormat = (value) => String(value).padStart(2, `0`);
+export const getTwoDigitFormat = (value) => String(value).padStart(2, `0`);
 
 const formatterOptionsTime = {
   hour12: false,
@@ -8,7 +8,7 @@ const formatterOptionsTime = {
 
 const TimeFormatter = new Intl.DateTimeFormat(`en-US`, formatterOptionsTime);
 
-const formatTime = (date) => TimeFormatter.format(date);
+export const formatTime = (date) => TimeFormatter.format(date);
 
 const formatterOptionsDate = {
   day: `numeric`,
@@ -17,12 +17,33 @@ const formatterOptionsDate = {
 
 const DateFormatter = new Intl.DateTimeFormat(`en-US`, formatterOptionsDate);
 
-const formatDate = (date) => DateFormatter.format(date);
+export const formatDate = (date) => DateFormatter.format(date);
 
-const isFirst = (index) => index === 0;
+export const isFirst = (index) => index === 0;
 
-const getFirst = (array) => array[0];
+export const getFirst = (array) => array[0];
 
-const getLast = (array) => array[array.length - 1];
+export const getLast = (array) => array[array.length - 1];
 
-export { formatTime, getTwoDigitFormat, formatDate, isFirst, getFirst, getLast };
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstElementChild;
+};
+
+export const render = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
