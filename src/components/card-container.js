b7beb853dd6625bfd90326d0f7762e9916d95678
Dar <ksen.vl.set@gmail.com>
2019-12-04 15:02:33 +0300

    Rewrite card-container.js on classes

--- src/components/card-container.js
+++ src/components/card-container.js
@@ -1,5 +1,29 @@
-export const getCardContainerTemplate = () => {
+import { createElement } from '../utils';
+
+const getCardContainerTemplate = () => {
   return (`
-    <ul class="trip-days js-trip-days"> </ul>
+    <ul class="trip-days js-trip-days"></ul>
   `);
 };
+
+export default class CardContainer {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return getCardContainerTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
