caebab242faa610f3064108f2af4e192e0354efb
Dar <ksen.vl.set@gmail.com>
2019-12-10 23:28:40 +0300

    Inherits a no-day-message component from an abstract class

--- src/components/no-cards-message.js
+++ src/components/no-cards-message.js
@@ -1,27 +1,11 @@
-import { createElement } from '../utils';
+import AbstractComponent from './abstract-component';
 
 const getNoCardsMessageTemplate = () => {
   return `<p class="trip-events__msg">Click New Event to create your first point</p>`;
 };
 
-export default class NoCardsMessage {
-  constructor() {
-    this._element = null;
-  }
-
+export default class NoCardsMessage extends AbstractComponent {
   getTemplate() {
     return getNoCardsMessageTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
