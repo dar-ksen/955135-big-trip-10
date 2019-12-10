03c14056e6391e0edab32bca90ce7ec25c2c41de
Dar <ksen.vl.set@gmail.com>
2019-12-10 23:30:20 +0300

    Inherits a sort-card component from an abstract class

--- src/components/sort-cards.js
+++ src/components/sort-cards.js
@@ -1,4 +1,4 @@
-import { createElement } from '../utils';
+import AbstractComponent from './abstract-component';
 
 const sortCardTemplate = () => {
   return (`
@@ -12,24 +12,8 @@ const sortCardTemplate = () => {
   );
 };
 
-export default class SortCards {
-  constructor() {
-    this._element = null;
-  }
-
+export default class SortCards extends AbstractComponent {
   getTemplate() {
     return sortCardTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
