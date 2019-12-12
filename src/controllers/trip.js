8ddb235df76d38af947961e766aafddad03fa592
Dar <ksen.vl.set@gmail.com>
2019-12-12 20:36:45 +0300

    Set up card sorting

--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -1,6 +1,7 @@
-import SortComponent from '../components/sort';
+import SortComponent, { SortType } from '../components/sort';
 import DayListComponent from '../components/day-list';
 import DayComponent from '../components/day';
+import SortCardsComponent from '../components/sort-cards';
 
 import NoCardsMessageComponent from '../components/no-cards-message';
 
@@ -39,6 +40,27 @@ const renderCard = (cardListElement, card) => {
   renderComponent(cardListElement, cardComponent);
 };
 
+const renderDays = (dayListElement, cards) => {
+  const days = getUnique(cards.map((card) => getDate(card.startTime)));
+
+  days.forEach((day) => {
+    const dayComponent = new DayComponent(day);
+    renderComponent(dayListElement, dayComponent);
+    const eventList = dayComponent.getElement().querySelector(`.js-trip-events__list`);
+    cards
+      .filter((card) => getDate(card.startTime) === day)
+      .forEach((card) => renderCard(eventList, card));
+  });
+};
+
+const renderSortCards = (dayListElement, cards) => {
+  const sortCardsComponent = new SortCardsComponent();
+  renderComponent(dayListElement, sortCardsComponent);
+  const eventList = sortCardsComponent.getElement().querySelector(`.js-trip-events__list`);
+  cards
+    .forEach((card) => renderCard(eventList, card));
+};
+
 export default class TripController {
   constructor(container) {
     this._container = container;
@@ -62,15 +84,33 @@ export default class TripController {
 
     renderComponent(this._container, this._dayListComponent);
 
-    const days = getUnique(cards.map((card) => getDate(card.startTime)));
+    renderDays(dayListElement, cards);
 
-    days.forEach((day) => {
-      const dayComponent = new DayComponent(day);
-      renderComponent(dayListElement, dayComponent);
-      const eventList = dayComponent.getElement().querySelector(`.js-trip-events__list`);
-      cards
-        .filter((card) => getDate(card.startTime) === day)
-        .forEach((card) => renderCard(eventList, card));
+    this._sortComponent.setSortTypeChangeHandlet((sortType) => {
+      let sortedCard = [];
+
+      switch (sortType) {
+        case SortType.TIME:
+          sortedCard = cards.slice().sort((a, b) => (b.endTime - b.startTime) - (a.endTime - a.startTime));
+          break;
+        case SortType.PRICE:
+          sortedCard = cards.slice().sort((a, b) => b.price - a.price);
+          break;
+        case SortType.DEFAULT:
+          sortedCard = cards;
+          break;
+        default :
+          sortedCard = cards;
+      }
+
+      dayListElement.innerHTML = ``;
+
+      if (sortType === SortType.DEFAULT) {
+        renderDays(dayListElement, sortedCard);
+      } else {
+        renderSortCards(dayListElement, sortedCard);
+      }
+
     });
   }
 }
