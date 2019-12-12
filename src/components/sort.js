1d17ebed05cb767d34e735dc795c8ea08da974e7
Dar <ksen.vl.set@gmail.com>
2019-12-12 19:32:03 +0300

    Add sort types

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,49 +1,67 @@
 import AbstractComponent from './abstract-component';
 
-import { isFirst } from '../utils/common';
-
-const sortItems = [
-  {
-    id: `event`,
-    title: `Event`
-  },
-  {
-    id: `time`,
-    title: `Time`
-  },
-  {
-    id: `price`,
-    title: `Price`
-  }
-];
-
-const getSortListTemplate = (items) => items.map(({ id, title }, index) => `
-<div class="trip-sort__item  trip-sort__item--${id}">
-  <input id="sort-${id}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${id}" ${isFirst(index) ? `checked` : ``}>
-  <label class="trip-sort__btn" for="sort-${id}">
-  ${title}
-  ${isFirst(index) ? `` : `
-    <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
-    <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
-    </svg>`}
-  </label>
-</div>
-`).join(`\n`);
+const SortType = {
+  TIME: `time`,
+  PRICE: `price`,
+  DEFAULT: `default`,
+};
 
 const getSortTemplate = () => {
-  const sortListTemplate = getSortListTemplate(sortItems);
   return (`
-  <form class="trip-events__trip-sort  trip-sort js-trip-sort" action="#" method="get">
-    <span class="trip-sort__item  trip-sort__item--day">Day</span>
+    <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+      <span class="trip-sort__item  trip-sort__item--day"></span>
 
-    ${sortListTemplate}
+      <div class="trip-sort__item  trip-sort__item--event">
+        <input
+          id="sort-event"
+          data-sort-type="${SortType.DEFAULT}"
+          class="trip-sort__input visually-hidden"
+          type="radio" name="trip-sort"
+          value="sort-event"
+          checked
+        >
+        <label class="trip-sort__btn" for="sort-event">Event</label>
+      </div>
 
-    <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
-  </form>
+      <div class="trip-sort__item  trip-sort__item--time">
+        <input
+          id="sort-time"
+          data-sort-type="${SortType.TIME}"
+          class="trip-sort__input  visually-hidden"
+          type="radio"
+          name="trip-sort"
+          value="sort-time"
+        >
+        <label class="trip-sort__btn  trip-sort__btn--active  trip-sort__btn--by-increase" for="sort-time">
+          Time
+        </label>
+      </div>
+
+      <div class="trip-sort__item  trip-sort__item--price">
+        <input
+          id="sort-price"
+          data-sort-type="${SortType.PRICE}"
+          class="trip-sort__input visually-hidden"
+          type="radio" name="trip-sort"
+          value="sort-price"
+        >
+        <label class="trip-sort__btn" for="sort-price">
+          Price
+        </label>
+      </div>
+
+      <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
+    </form>
   `);
 };
 
 export default class Sort extends AbstractComponent {
+  constructor() {
+    super();
+
+    this._currentSortType = SortType.DEFAULT;
+  }
+
   getTemplate() {
     return getSortTemplate();
   }
