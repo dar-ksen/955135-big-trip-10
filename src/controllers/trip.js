9093cd5c26fb6e9fa15cf6d9ffbb9435bc6b2ede
Dar <ksen.vl.set@gmail.com>
2019-12-17 18:25:06 +0300

    Configure trip-controller

--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -6,92 +6,129 @@ import PointController from './point.js';
 
 import NoEventsMessageComponent from '../components/no-events-message';
 
-import { getDate, getUnique } from '../utils/common';
+import {
+  getDate,
+  getUnique,
+  sortByDurationInDescendingOrder,
+  sortByPriceInDescendingOrder,
+  sortPurely } from '../utils/common';
 
 import { renderComponent } from '../utils/render';
 
-const sortByDurationInDescendingOrder = (a, b) => (b.endTime - b.startTime) - (a.endTime - a.startTime);
+const renderPoints = (eventList, points, onDataChange, onViewChange) => {
+  return points.map((pointData) => {
+    const pointController = new PointController(eventList, onDataChange, onViewChange);
+    pointController.render(pointData);
 
-const sortByPriceInDescendingOrder = (a, b) => b.price - a.price;
-
-const sortPurely = (collection, iterate) => collection.slice().sort(iterate);
-
-const renderDays = (dayListElement, events) => {
-  const days = getUnique(events.map((event) => getDate(event.startTime)));
-
-  days.forEach((day) => {
-    const dayComponent = new DayComponent(day);
-    renderComponent(dayListElement, dayComponent);
-    const eventList = dayComponent.getElement().querySelector(`.js-trip-events__list`);
-    events
-      .filter((event) => getDate(event.startTime) === day)
-      .forEach((event) => {
-        const pointController = new PointController(eventList);
-        pointController.render(event);
-      });
-  });
-};
-
-const renderSortEvents = (dayListElement, events) => {
-  const sortEventsComponent = new SortEventsComponent();
-  renderComponent(dayListElement, sortEventsComponent);
-  const eventList = sortEventsComponent.getElement().querySelector(`.js-trip-events__list`);
-  events.forEach((event) => {
-    const pointController = new PointController(eventList);
-    pointController.render(event);
+    return pointController;
   });
 };
 
 export default class TripController {
   constructor(container) {
     this._container = container;
+    this._points = [];
+    this._showedPointControllers = [];
+
+    this._onSortTypeChange = this._onSortTypeChange.bind(this);
+    this._onDataChange = this._onDataChange.bind(this);
+    this._onViewChange = this._onViewChange.bind(this);
+
 
     this._noEventsMessageComponent = new NoEventsMessageComponent();
     this._sortComponent = new SortComponent();
     this._dayListComponent = new DayListComponent();
+
+    this._sortComponent.setSortTypeChangeHandler(this._onSortTypeChange);
   }
 
-  render(events) {
-    if (events.length === 0) {
+  render(points) {
+    this._points = points;
+
+    if (this._points.length === 0) {
       renderComponent(this._container, this._noEventsMessageComponent);
       return;
     }
 
     renderComponent(this._container, this._sortComponent);
+    renderComponent(this._container, this._dayListComponent);
+
+    this._renderDays(this._points);
+  }
+
+  _renderSortEvents(points) {
+    const dayListElement = this._dayListComponent.getElement();
+    const sortEventsComponent = new SortEventsComponent();
+    renderComponent(dayListElement, sortEventsComponent);
+    const eventList = sortEventsComponent.getElement().querySelector(`.js-trip-events__list`);
+    const newTasks = renderPoints(eventList, points, this._onDataChange, this._onViewChange);
+    this._showedPointControllers = this._showedPointControllers.concat(newTasks);
+  }
+
+  _renderDays(points) {
+    const dayListElement = this._dayListComponent.getElement();
+
+    const days = getUnique(points.map((point) => getDate(point.startTime)));
+
+    days.forEach((day) => {
+      const dayComponent = new DayComponent(day);
+      renderComponent(dayListElement, dayComponent);
+
+      const eventList = dayComponent.getElement().querySelector(`.js-trip-events__list`);
+
+      const dayPoints = points.filter((point) => getDate(point.startTime) === day);
+      const newTasks = renderPoints(eventList, dayPoints, this._onDataChange, this._onViewChange);
+      this._showedPointControllers = this._showedPointControllers.concat(newTasks);
+    });
+  }
+
+  _onDataChange(pointController, oldData, newData) {
+    const index = this._points.findIndex((it) => it === oldData);
+
+    if (index === -1) {
+      return;
+    }
+
+    this._points = [].concat(this._points.slice(0, index), newData, this._points.slice(index + 1));
+
+    pointController.render(this._points[index]);
+  }
+
+  _onViewChange() {
+    this._showedPointControllers.forEach((it) => it.setDefaultView());
+  }
+
+  _onSortTypeChange(sortType) {
+    let sortedEvent = [];
 
     const dayListElement = this._dayListComponent.getElement();
 
-    renderComponent(this._container, this._dayListComponent);
-
-    renderDays(dayListElement, events);
-
-    this._sortComponent.setSortTypeChangeHandler((sortType) => {
-      let sortedEvent = [];
-
-      switch (sortType) {
-        case SortType.TIME: {
-          sortedEvent = sortPurely(events, sortByDurationInDescendingOrder);
-          break;
-        }
-        case SortType.PRICE: {
-          sortedEvent = sortPurely(events, sortByPriceInDescendingOrder);
-          break;
-        }
-        case SortType.DEFAULT:
-        default : {
-          sortedEvent = events;
-          break;
-        }
+    switch (sortType) {
+      case SortType.TIME: {
+        sortedEvent = sortPurely(this._points, sortByDurationInDescendingOrder);
+        break;
       }
-
-      dayListElement.innerHTML = ``;
-
-      if (sortType === SortType.DEFAULT) {
-        renderDays(dayListElement, sortedEvent);
-      } else {
-        renderSortEvents(dayListElement, sortedEvent);
+      case SortType.PRICE: {
+        sortedEvent = sortPurely(this._points, sortByPriceInDescendingOrder);
+        break;
       }
+      case SortType.DEFAULT:
+      default : {
+        sortedEvent = this._points;
+        break;
+      }
+    }
 
-    });
+    this._showedPointControllers = [];
+
+    dayListElement.innerHTML = ``;
+
+    if (sortType === SortType.DEFAULT) {
+      this._renderDays(sortedEvent);
+    } else {
+      this._renderSortEvents(sortedEvent);
+    }
+
   }
+
 }
