7e0d36404fe639f1196b4712c99bdbff294d5b6f
Dar <ksen.vl.set@gmail.com>
2019-12-27 08:20:50 +0300

    Add methods to the model

--- models/point-model.js
+++ models/point-model.js
@@ -2,6 +2,14 @@ class PointModel {
   constructor() {
     this._points = [];
   }
+
+  getPoints() {
+    return this._points;
+  }
+
+  setPoints(points) {
+    this._points = Array.from(points);
+  }
 }
 
 export { PointModel as default };
